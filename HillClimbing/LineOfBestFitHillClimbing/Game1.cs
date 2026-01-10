2d61c8a28ebdb6d20ab05d39785d885062485230
Ariana Dadgostar <ariana.dadgostar@gmail.com>
2026-01-10 14:41:37 -0800

    Fixed graph/points, working on line

--- HillClimbing/LineOfBestFitHillClimbing/Game1.cs
+++ HillClimbing/LineOfBestFitHillClimbing/Game1.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using System.Transactions;
 
 namespace LineOfBestFitHillClimbing
 {
+    public class Line
+    {
+        public int slope;
+        public int b;
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch spriteBatch;
         private Texture2D pixel;
-        private int[] points;
+        private (int x, int y)[] points;
         private int index;
+        private MouseState previousMouseState;
 
         const int LINENUM = 30;
+        const int XLENGTH = 450;
+        const int YLENGTH = 450;
 
         public Game1()
         {
@@ -48,16 +57,24 @@ namespace LineOfBestFitHillClimbing
             pixel = new Texture2D(GraphicsDevice, 1, 1);
             pixel.SetData(new[] { Color.White });
 
-            points = new int[100];
+            points = new (int x, int y)[100];
             index = 0;
 
             // TODO: use this.Content to load your game content here
+        }
+
+        public void MutateLine(ref int slope, ref int b)
+        {
+            Random random = new Random();
+            slope += random.Next(-1, 2);
+            b += random.Next(-1, 2);
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
 
             // TODO: Add your update logic here
 
@@ -86,6 +103,18 @@ namespace LineOfBestFitHillClimbing
             spriteBatch.Draw(pixel,
                              new Rectangle(x, y, 15, 15),
                              Color.IndianRed);
+            points[index] = (x, y);
+            index++;
+        }
+
+        public void DrawPoints((int x, int y)[] points)
+        {
+            for (int i = 0; i < index; i ++)
+            {
+                spriteBatch.Draw(pixel,
+                                 new Rectangle(points[i].x, points[i].y, 15, 15),
+                                 Color.IndianRed);
+            }
         }
 
         protected override void Draw(GameTime gameTime)
@@ -93,7 +122,19 @@ namespace LineOfBestFitHillClimbing
             spriteBatch.Begin();
             GraphicsDevice.Clear(Color.DarkOliveGreen);
 
+
+            MouseState currentMouseState = Mouse.GetState();
+
+            if (currentMouseState.LeftButton == ButtonState.Pressed &&
+                previousMouseState.LeftButton == ButtonState.Released)
+            {
+                AddPoints(currentMouseState.X, currentMouseState.Y);
+            }
+
+            previousMouseState = currentMouseState;
+
             DrawGraph(450, 450);
+            DrawPoints(points);
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
