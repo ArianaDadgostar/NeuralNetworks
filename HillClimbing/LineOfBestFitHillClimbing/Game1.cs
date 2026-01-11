12f8aece45d6bb7fe9e6765963c47badc08ce3a6
Ariana Dadgostar <ariana.dadgostar@gmail.com>
2026-01-11 10:02:14 -0800

    fixed line creation, debugging fit calc

--- HillClimbing/LineOfBestFitHillClimbing/Game1.cs
+++ HillClimbing/LineOfBestFitHillClimbing/Game1.cs
@@ -10,6 +10,12 @@ namespace LineOfBestFitHillClimbing
     {
         public int slope;
         public int b;
+
+        public Line(int slope, int b)
+        {
+            this.slope = slope;
+            this.b = b;
+        }
     }
     public class Game1 : Game
     {
@@ -19,6 +25,7 @@ namespace LineOfBestFitHillClimbing
         private (int x, int y)[] points;
         private int index;
         private MouseState previousMouseState;
+        private Line line;
 
         const int LINENUM = 30;
         const int XLENGTH = 450;
@@ -31,16 +38,16 @@ namespace LineOfBestFitHillClimbing
             IsMouseVisible = true;
         }
 
-        public int CalculateFit(int[] points, int slope, int b)
+        public int CalculateFit((int x, int y)[] points, int slope, int b)
         {
             int result = 0;
             for (int i = 0; i < points.Length; i++)
             {
-                int line = (slope * points[i]) + b;
-                result += points[i] - line;
+                int lineResult = (slope * points[i].x) + b;
+                result += points[i].y - lineResult;
             }
 
-            return result / points.Length;
+            return Math.Abs(result / points.Length);
         }
 
         protected override void Initialize()
@@ -60,14 +67,23 @@ namespace LineOfBestFitHillClimbing
             points = new (int x, int y)[100];
             index = 0;
 
+            Random random = new Random();
+
+            line = new Line(random.Next(1, 30), random.Next(1, 30));
+
             // TODO: use this.Content to load your game content here
         }
 
         public void MutateLine(ref int slope, ref int b)
         {
             Random random = new Random();
-            slope += random.Next(-1, 2);
-            b += random.Next(-1, 2);
+            int newSlope = slope + random.Next(-1, 2);
+            int newB = b + random.Next(-1, 2);
+
+            // if(CalculateFit(points, slope, b) < CalculateFit(points, newSlope, newB)) return;
+
+            slope = newSlope;
+            b = newB;
         }
 
         protected override void Update(GameTime gameTime)
@@ -75,11 +91,14 @@ namespace LineOfBestFitHillClimbing
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+                MutateLine(ref line.slope, ref line.b);
 
             // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
+
+        #region Drawing Logic
 
         public void DrawGraph(int xLength, int yLength)
         {
@@ -107,7 +126,7 @@ namespace LineOfBestFitHillClimbing
             index++;
         }
 
-        public void DrawPoints((int x, int y)[] points)
+        public void DrawPoints()
         {
             for (int i = 0; i < index; i ++)
             {
@@ -116,6 +135,18 @@ namespace LineOfBestFitHillClimbing
                                  Color.IndianRed);
             }
         }
+
+        public void DrawLine()
+        {
+            for(int i = 0; i <= XLENGTH; i ++)
+            {
+                spriteBatch.Draw(pixel,
+                                 new Rectangle(i, (line.slope * i) + line.b, 5, 5),
+                                 Color.DarkBlue);
+            }
+        }
+
+        #endregion
 
         protected override void Draw(GameTime gameTime)
         {
@@ -134,7 +165,8 @@ namespace LineOfBestFitHillClimbing
             previousMouseState = currentMouseState;
 
             DrawGraph(450, 450);
-            DrawPoints(points);
+            DrawPoints();
+            DrawLine();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
