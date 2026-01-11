66a87189df8fd50a7c12e3405244efff5c9fae2a
Ariana Dadgostar <ariana.dadgostar@gmail.com>
2026-01-11 11:59:05 -0800

    Added Perceptrons + randomization.

---
+++ Perceptrons/Perceptrons/Perceptron.cs
@@ -0,0 +1,29 @@
+﻿namespace Perceptrons
+{
+    public class Perceptron
+    {
+        double[] weights;
+        double bias;
+
+        public Perceptron(int numOfInputs)
+        {
+            weights = new double[numOfInputs];
+        }
+
+        public Perceptron(int numOfInputs, double bias)
+        {
+            weights = new double[numOfInputs];
+            this.bias = bias;
+        }
+
+        public void Randomize(Random random, int min, int max)
+        {
+            for (int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = random.Next(min, max);
+            }
+
+            bias = random.Next(min, max);
+        }
+    }
+}
